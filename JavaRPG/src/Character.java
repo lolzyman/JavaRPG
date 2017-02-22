1ea06f3bedee78328dbf6d06fa503b4d3e5b6762
lolzyman <markkent777@gmail.com>
2017-02-22 00:29:43 -0500

    Class for the chacter information

---
+++ JavaRPG/src/Character.java
@@ -0,0 +1,49 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Character {
+
+	int x = 0;                     //x position
+    int y = 0;                     //y position
+    private BufferedImage image;
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public Character(int xx, int yy){
+		 x = xx;
+		 y = yy;
+		 image();
+	}
+	
+	void image(){
+		try
+        {
+			image = ImageIO.read(getClass().getResourceAsStream("character.png"));
+        }
+        
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+	}
+}

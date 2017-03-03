47672dcaaa765122e56f837cc6882af8cb1f5ad6
lolzyman <markkent777@gmail.com>
2017-03-02 22:44:29 -0500

    Added documentation for this superclass

--- JavaRPG/src/FileManager.java
+++ JavaRPG/src/FileManager.java
@@ -1,29 +1,38 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+//This is a super class used by the LoadMap Class and by the ImportCSV class. This class contains
+//two methods. getFile finds the file with the apporiate file name and returns a File class for it.
+//setUpScanner sets up a scanner that targets the selected file 
 public class FileManager {
+	//empty constructor
 	public FileManager(){
 		
 	}
+	//This method tries to find a file with the name of the string parameter
 	File getFile(String fileName){
+		//initiates a null File class
 		File file = null;
 		//Not sure what this line of code does honestly
 		ClassLoader classLoader = getClass().getClassLoader();
-		
 		//tries to locate said file. If the file isn't found file remains null
 		try{
+			//tries to locate the file
 			file = new File(classLoader.getResource(fileName).getFile());
 		} catch(NullPointerException e){
+			//debugging error message
 			System.out.println("File wasn't found2");
 		}
 		return file;
 	}
+	//this method sets up a scanner to read from the target file
 	Scanner setUpScanner(File file){
+		//initiates a null Scanner class
 		Scanner input = null;
 		if(file != null){
 			//tries to point the scanner to the file
 			try {
+				//tries to point the scanner to the file
 				input = new Scanner(file);
 			} catch (NullPointerException e) {
 				e.printStackTrace();
@@ -45,6 +54,7 @@ public class FileManager {
 				felix.close();
 				return null;
 			}
+			//closes the scanner
 			felix.close();
 		}
 		return input;
